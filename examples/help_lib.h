b7b81284949dbc8cdaa48d2f786d203c52ba2ed4
uselessgoddess <dodickgod@gmail.com>
2021-06-04 20:33:58 +0300

    Fixed print adaptor

--- examples/help_lib.h
+++ examples/help_lib.h
@@ -1,71 +1,14 @@
 #ifndef HELP_LIB
 #define HELP_LIB
 
-template <std::ranges::range T>
-struct const_enumerate_impl
-{
-    using TItem = std::ranges::range_value_t<T>;
-    using TIterator = std::ranges::iterator_t<const T>;
-    const T& container;
-
-    class iterator
-    {
-        TIterator it;
-        size_t counter = 0;
-
-    public:
-        iterator(TIterator it) :
-            it(it)
-        {
-        }
-
-        iterator& operator++()
-        {
-            ++it;
-            ++counter;
-            return *this;
-        }
-
-        bool operator!=(iterator other)
-        {
-            return it != other.it;
-        }
-
-        std::tuple<std::size_t, TItem> operator*()
-        {
-            return { counter, *it };
-        }
-    };
-
-public:
-    const_enumerate_impl(const T& t) : container(t) {}
-
-    iterator begin()
-    {
-        return iterator(std::ranges::begin(container));
-    }
-
-    iterator end()
-    {
-        return iterator(std::ranges::end(container));
-    }
-};
-
-template<typename Collection>
-const_enumerate_impl(Collection) -> const_enumerate_impl<Collection>;
-
-declare enumerate = [](std::ranges::range auto&& collection)
-{
-    return const_enumerate_impl(collection);
-};
-
 declare print = [](const std::ranges::range auto& collection) -> decltype(collection)
 {
     std::cout << "[";
-    for (auto [index, item] : collection | enumerate())
+
+    for (const auto& item : collection)
     {
         std::cout << item;
-        if (index != std::ranges::size(collection) - 1)
+        if (&item != &*(std::ranges::end(collection) - 1))
             std::cout << ", ";
     }
     std::cout << "]";
